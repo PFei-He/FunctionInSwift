1508ba33c32278363dc21de8ebfa6c4d7a728673
PFei_He <498130877@qq.com>
2014-10-13 15:31:46 +0800

    Initial Commit

---
+++ FunctionInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FunctionInSwift
+//
+//  Created by PFei_He on 14-10-13.
+//  Copyright (c) 2014年 PFei_He. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
